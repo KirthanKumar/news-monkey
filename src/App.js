6c0a5ebf5348335ca7ccc59da54dc3d53a3bcd2c
KirthanKumar <118553301+KirthanKumar@users.noreply.github.com>
2024-03-27 02:13:59 +0530

    #22

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React, { Component } from 'react'; // rcc
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
