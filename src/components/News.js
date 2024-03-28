92d5c9c390f7bfdbebb78d901ef614c6f5d98bbb
KirthanKumar <118553301+KirthanKumar@users.noreply.github.com>
2024-03-28 19:03:52 +0530

    #34 - REACT COMPONENT LIFECYCLE AND LIFECYCLE METHODS

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react"; // rce
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 // rce
 export class News extends Component {
@@ -10,31 +9,35 @@ export class News extends Component {
     country: "in",
     pageSize: 8,
     category: "general",
-  }
-
+  };
 
   static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-  }
+  };
 
-  constructor() {
-    super();
+  capitalizeFirstletter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  constructor(props) {
+    super(props);
     // console.log("Hello I am a constructor from News component");
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     };
+    document.title = `${this.capitalizeFirstletter(
+      this.props.category
+    )} - NewsMonkey`;
   }
 
   async componentDidMount() {
     // console.log("componentDidMount");
     // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c4c450c08e904226a8ff20ed56c63844&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-
     // this.setState({ loading: true });
-    
     // let data = await fetch(url);
     // let parsedData = await data.json();
     // // console.log(parsedData);
@@ -42,8 +45,7 @@ export class News extends Component {
     //   articles: parsedData.articles,
     //   totalResults: parsedData.totalResults,
     //   loading: false,
-    // }); 
-
+    // });
     this.updateNews(this.state.page);
   }
 
@@ -75,7 +77,7 @@ export class News extends Component {
     //   }&apiKey=c4c450c08e904226a8ff20ed56c63844&page=${
     //     this.state.page + 1
     //   }&pageSize=${this.props.pageSize}`;
-      
+
     //   this.setState({ loading: true });
 
     //   let data = await fetch(url);
@@ -101,9 +103,9 @@ export class News extends Component {
     // }&apiKey=c4c450c08e904226a8ff20ed56c63844&page=${
     //   this.state.page - 1
     // }&pageSize=${this.props.pageSize}`;
-    
+
     // this.setState({ loading: true });
-    
+
     // let data = await fetch(url);
     // let parsedData = await data.json();
     // // console.log(parsedData);
@@ -121,29 +123,33 @@ export class News extends Component {
     // console.log("render");
     return (
       <div className="container my-3">
-        <h2 className="text-center" style={{margin:"35px 0px"}}>NewsMonkey - Top Headlines</h2>
+        <h2 className="text-center" style={{ margin: "35px 0px" }}>
+          NewsMonkey - Top {this.capitalizeFirstletter(this.props.category)}{" "}
+          Headlines
+        </h2>
         {this.state.loading && <Spinner />}
         <div className="row">
-          {!(this.state.loading) && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  // title={element.title ? element.title.slice(0, 40) : ""}
-                  title={element.title ? element.title : ""} // no need to slice it looks good without slicing
-                  // description={(element.title.length+element.description.length > 100)?element.description.slice(0, 100-element.title.length):element.description}
-                  description={
-                    // element.description ? element.description.slice(0, 80) : ""
-                    element.description ? element.description : ""
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    // title={element.title ? element.title.slice(0, 40) : ""}
+                    title={element.title ? element.title : ""} // no need to slice it looks good without slicing
+                    // description={(element.title.length+element.description.length > 100)?element.description.slice(0, 100-element.title.length):element.description}
+                    description={
+                      // element.description ? element.description.slice(0, 80) : ""
+                      element.description ? element.description : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
