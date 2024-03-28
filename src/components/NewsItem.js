ac83703ee24876801857da4f9ed5c7987802e6e4
KirthanKumar <118553301+KirthanKumar@users.noreply.github.com>
2024-03-28 11:21:34 +0530

    #27 - USING FETCH API FOR HTTP CALL (version 1)

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,11 +11,26 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." height="161 px" width="286 px"/>
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://lh3.googleusercontent.com/J6_coFbogxhRI9iM864NL_liGXvsQp2AupsKei7z0cNNfDvGUmWUy20nuUhkREQyrpY4bEeIBuc=s0-w300-rw"
+            }
+            className="card-img-top"
+            alt="..."
+            height="161 px"
+            width="286 px"
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} className="btn btn-sm btn-primary">
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-primary"
+            >
               Read More
             </a>
           </div>
